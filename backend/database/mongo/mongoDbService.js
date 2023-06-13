a7380b259eddd4eec2ebb54ecba94ad5167fd26b
stjonas <jonasstoegerer@yahoo.at>
2023-06-13 23:23:15 +0200

    first version of article report

--- backend/database/mongo/mongoDbService.js
+++ backend/database/mongo/mongoDbService.js
@@ -11,20 +11,15 @@ class MongoDbService {
 
   async importData() {
     try {
-      //await this.client.connect();
       await importMongoDbData(this.database);
       return {message: "Data imported successfully"};
     } catch (error) {
       console.error("Error importing data: " + error);
       throw error;
     }
-    finally {
-      //await this.client.close();
-    }
   }
 
   async getLatestArticles() {
-   // await this.client.connect();
     try {
         const articles = await this.database
         .collection("article")
@@ -40,10 +35,6 @@ class MongoDbService {
     } catch (error) {
       console.error("Error retrieving articles:", error);
       throw error;
-    }
-    finally
-    {
-      //await this.client.close();
     }
   }
 
@@ -209,7 +200,46 @@ async insertComment(article_id, user_id, comment_content) {
   }
 
   async getArticleReport() {
-    return[];
+    try {
+        const oneYearAgo = new Date(Date.now() - 365 * 24 * 60 * 60 * 1000); // One year ago
+  
+          const journalistId = "64876ff5aa899efc04a3d876";
+          
+  
+          const journalists = await this.database
+              .collection("journalist")
+              .find({})
+              .toArray();
+
+  
+          const mostActiveJournalists = await this.database
+              .collection("article")
+              .find({})
+              .sort({ publish_time: 1, })
+              .limit(10);
+    
+        const report = await Promise.all(
+          journalists.map(async (journalist) => {
+            const recentArticle = await this.database
+              .collection("article")
+              .find({ publish_time: { $gte: oneYearAgo } })
+              .sort({ publish_time: 1 })
+              .limit(2);
+              console.log("recentArticle", recentArticle);
+            return {
+              employee_id: journalist.employee_id,
+              fullName: `${journalist.last_name} ${journalist.first_name}`,
+              publishedArticles: journalist.publishedArticles,
+              recentArticleTitle: recentArticle ? recentArticle.title : null,
+              username: journalist.user.username
+            };
+          })
+        );
+          return report;
+      } catch (error) {
+        console.error('Error retrieving article report:', error);
+        throw error;
+      }
   }
   
   
